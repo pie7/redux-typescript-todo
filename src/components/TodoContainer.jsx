592fd3c1bc878b12ff6f99fac5f0b61ca47bfc18
pie7 <j3pxy6@gmail.com>
2020-04-09 18:08:04 +0800

    Add TodoContainer Component

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { addTask, editTask, deleteTask,  toggleTask} from "../reducers/todo";
+import TodoCard from "./TodoCard";
+import AddButton from "./AddButton";
+import "./todo.scss";
+
+const TodoContainer = ({todos, toggleTask}) => {
+    return (
+        <div className="todo__container">
+            <header className="todo__title">
+                ToDoList
+            </header>
+            {todos &&
+                todos.map(todo =>
+                    <TodoCard
+                        key={todo.id}
+                        id={todo.id}
+                    />
+                )
+            }
+            <AddButton />
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTask: () => dispatch(addTask),
+        editTask: () => dispatch(editTask),
+        deleteTask: () => dispatch(deleteTask),
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoContainer)
